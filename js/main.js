25ca0a90ca143fcdd188bcecb709e6cdc2b116fa
Machuca Mirtha <machucamirta@gmail.com>
2024-10-03 19:16:29 -0300

    Segunda-preentrega-MachucaMirtha

---
+++ js/main.js
@@ -0,0 +1,97 @@
+class Productos {
+
+    constructor(nombre,cantidad,color,talle) {
+        this.nombre = nombre
+        this.cantidad = cantidad
+        this.color = color
+        this.talle = talle
+    }
+
+}
+
+let remeras = [
+    new Productos('Principito', 3, 'Blanco', 's'),
+    new Productos('Hello', 3, 'Blanco', 'l'),
+    new Productos('Kala', 2, 'Negro', 's'),
+    new Productos('Karla', 2, 'Negro', 's'),
+    new Productos('Kika', 1, 'Fucsia', 'm'),
+    new Productos('Hello', 3, 'Bordo', 'l'),
+];
+let carrito=[];
+
+alert('Bienvenido a la Tienda Online de Dhaka Clothing');
+
+while(true){
+    // le damos la opcion de elegir una de las 3 opciones
+    let respuesta = validar_num('Elige la operación que deseas realizar: \n 1: Agregar Productos al carrito (1) \n 2: Listar Productos agregados (2) \n 3: Eliminar Producto (3)',1,3);
+
+    if (respuesta==1){
+
+        agregarProducto();
+
+    }else if (respuesta == 2){
+
+        mostrarProductos();
+
+    }else{
+        eliminarProducto();
+    }
+
+    if(!confirm('¿Desea continuar operando en el sistema?')){
+        break;
+    }
+}
+
+
+function validar_num(texto,min,max){
+    let num = Number(prompt(texto))
+    while(isNaN(num) || num < min || num > max) { 
+        alert('Ingrese el dato que corresponde');
+        num = Number(prompt(texto));
+    }
+    return num;
+}
+
+function agregarProducto() {
+    //mostrams lista y le decimos que ingrese nombre del prod
+    let mensaje = 'Productos disponibles:\n';
+    remeras.forEach((prod, index) => {
+        mensaje += `${index}. ${prod.nombre} - ${prod.cantidad} disponibles\n`;
+    });
+        let agregar = prompt(mensaje + '¿Ingrese el nombre del producto que desea agregar');
+        //controlamos si esta el prod ,si hay agregamos en el array carrito
+        let verificar = remeras.find((producto) => producto.nombre.toLowerCase() === agregar.toLowerCase());
+    
+        if (verificar){
+            carrito = carrito.push(agregar);
+            alert('producto agregado correctamente');
+            console.log(carrito)
+        }else{
+            alert('ingrese un producto existente');
+        }
+}
+
+function mostrarProductos() {
+    //verif si hay prod para mostrar de lo contrario tiramos mje que no hay prod
+    if (carrito.length === 0) {
+        alert('No hay productos en el carrito.');
+    } else {
+        let mensaje = 'Productos en el carrito:\n';
+        carrito.forEach((prod, index) => {
+            mensaje += `${index + 1}. ${prod.nombre} - ${prod.cantidad} unidades\n`;
+        });
+        alert(mensaje);
+    }
+}
+
+function eliminarProducto(){
+    let eliminar = prompt(mensaje + '¿Ingrese el nombre del producto que desea eliminar');
+    verificar = carrito.find((producto) => producto.nombre.toLowerCase() === eliminar.toLowerCase());
+
+    if (verificar){
+        carrito = carrito.filter(producto => producto.nombre !== eliminar);
+        alert('El producto se elimino correctamente');
+    }else{
+        alert('Ingrese un producto existente');
+    }
+}
